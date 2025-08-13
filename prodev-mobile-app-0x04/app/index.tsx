a8354c8e4b297eca25fc84e91aac2749272bd6e6
Joseph Wanjohi <joseph.mwai.wanjohi@.com>
2025-08-13 06:45:21 +0300

    fix: Update background image file extension from .jpg to .png and adding a fullstop to styles.smallText

--- prodev-mobile-app-0x04/app/index.tsx
+++ prodev-mobile-app-0x04/app/index.tsx
@@ -66,7 +66,7 @@ export default function Index() {
     <SafeAreaProvider>
       <SafeAreaView style={{flex: 1}}>
         <ImageBackground
-          source={require("@/assets/images/background-image.jpg")}
+          source={require("@/assets/images/background-image.png")}
           style={styles.background}
           resizeMode="cover"
         >
